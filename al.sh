2e2661e9e7b90152e30e49f7390cb2f17a42dc68
Harsh Singh <hsingh23@illinois.edu>
2012-10-16 17:52:12 -0500

    oi

--- al.sh
+++ al.sh
@@ -7,9 +7,7 @@ killall amarok >/dev/null 2>&1 & disown $!
 killall vlc >/dev/null 2>&1 & disown $!
 t=$[ ( $RANDOM % 5 )  + 1 ]
 sleep $t
-find ~/.config/al ~ ~/Downloads -maxdepth 1 -name '*.wav' -o -name
-'*.ogg' -o -name '*.mp3' | sort -R |tail -1 | xargs -I{} mplayer {}
->/dev/null 2>&1 & disown $!
+find ~/.config/al ~ ~/Downloads -maxdepth 1 -name '*.wav' -o -name '*.ogg' -o -name '*.mp3' | sort -R |tail -1 | xargs -I{} mplayer {} >/dev/null 2>&1 & disown $!
 for i in {1..30}
 do
     /usr/bin/amixer sset 'Auto-Mute Mode',0 Disabled >/dev/null 2>&1 & disown $!
@@ -20,5 +18,3 @@ do
 done & disown $!
 sleep 14
 /usr/bin/amixer sset 'Auto-Mute Mode',0 Enabled >/dev/null 2>&1 & disown $!
-/usr/bin/amixer -c 0 set Speaker,0 100% on >/dev/null 2>&1 & disown $!
-/usr/bin/amixer -c 0 set Master,0 80% >/dev/null 2>&1 & disown $!
