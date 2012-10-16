8686b4bb2e04a711bc77cfd93879eebfae67e823
Harsh Singh <hsingh23@illinois.edu>
2012-10-16 17:37:55 -0500

    wat

--- al.sh
+++ al.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# killall pianobar >/dev/null 2>&1 & disown $!
 killall pithos >/dev/null 2>&1 & disown $!
 killall mplayer >/dev/null 2>&1 & disown $!
 killall banshee >/dev/null 2>&1 & disown $!
@@ -8,13 +7,15 @@ killall amarok >/dev/null 2>&1 & disown $!
 killall vlc >/dev/null 2>&1 & disown $!
 t=$[ ( $RANDOM % 5 )  + 1 ]
 sleep $t
-find ~/.config/al ~ ~/Downloads -maxdepth 1 -name '*.wav' -o -name '*.ogg' -o -name '*.mp3' | sort -R |tail -1 | xargs -I{} mplayer {} >/dev/null 2>&1 & disown $!
+find ~/.config/al ~ ~/Downloads -maxdepth 1 -name '*.wav' -o -name
+'*.ogg' -o -name '*.mp3' | sort -R |tail -1 | xargs -I{} mplayer {}
+>/dev/null 2>&1 & disown $!
 for i in {1..30}
 do
-    /usr/bin/amixer sset 'Auto-Mute Mode',0 Disabled >/dev/null 2>&1
-    /usr/bin/amixer -D pulse set Master 1+ unmute >/dev/null 2>&1
-    /usr/bin/amixer -c 0 set Speaker,0 100% on >/dev/null 2>&1
-    /usr/bin/amixer -c 0 set Master,0 100% >/dev/null 2>&1
+    /usr/bin/amixer sset 'Auto-Mute Mode',0 Disabled >/dev/null 2>&1 & disown $!
+    /usr/bin/amixer -D pulse set Master 1+ unmute >/dev/null 2>&1 & disown $!
+    /usr/bin/amixer -c 0 set Speaker,0 100% on >/dev/null 2>&1 & disown $!
+    /usr/bin/amixer -c 0 set Master,0 100% >/dev/null 2>&1 & disown $!
     sleep 1
 done & disown $!
 sleep 14
